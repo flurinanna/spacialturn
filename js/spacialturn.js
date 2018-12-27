fa665f8efaf82052293f2f80945635b29cb55ea0
anna kälin <kaeliann@students.zhaw.ch>
2018-12-27 15:32:03 +0100

    set buildpack

--- js/spacialturn.js
+++ js/spacialturn.js
@@ -6,6 +6,22 @@ $(function () {
         el = $(el);
         el.click(function (event) {
             $(".contentWindow").hide();
+             $(".contentWindow." + el.data("nav")).show();
+            if(el.data("nav") === "projects") {
+                openChoosenProjectWindow(el.data("subwindow"))
+            }
+
+        })
+    })
+})
+
+$(function openChoosenProjectWindow(subwindow) {
+    var projectNames = $(".project");
+    projectNames.each(function (i, el) {
+        el = $(el);
+        el.click(function (event) {
+            $(".contentWindow").hide();
+            $(".contentWindow." + whichWindow + (subwindow ? "." + subwindow : "")).show();
             $(".contentWindow." + el.data("nav")).show();
             if(el.data("nav") === "about") {
             }
